23989821d2350396adeb4dbfdad753c60e8839ae
keunykyeongKim <woodeekim@gmail.com>
2020-06-17 23:56:14 +0900

    [Update java-basic] Colltections Framework - ArrayList(1)

---
+++ java-basic/src/main/java/com/woodee/collections/ArrayListEx01.java
@@ -0,0 +1,51 @@
+package com.woodee.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListEx01 {
+    public static void main(String[] args) {
+        ArrayList listOne = new ArrayList(10);
+        listOne.add(new Integer(5));
+        listOne.add(new Integer(4));
+        listOne.add(new Integer(2));
+        listOne.add(new Integer(0));
+        listOne.add(new Integer(1));
+        listOne.add(new Integer(3));
+
+        // List subList(int f, int t) fromIndex부터 toIndex사이에 저장된 객체를 반환
+        ArrayList listTwo = new ArrayList(listOne.subList(1, 4));
+        print(listOne, listTwo);
+
+        // List를 정렬
+        Collections.sort(listOne);
+        Collections.sort(listTwo);
+        print(listOne, listTwo);
+
+        System.out.println("listOne.containsAll(listwo) : " + listOne.containsAll(listTwo));
+
+        listTwo.add(3, "A");
+        print(listOne, listTwo);
+
+        listTwo.set(3,"AA");
+        print(listOne, listTwo);
+
+        // list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제
+        System.out.println("listOne.retainAll(listTwo) : " + listOne.retainAll(listTwo));
+        print(listOne, listTwo);
+
+        // list2에서 list1에 포함된 객체들을 삭제한다.
+        for (int i = listTwo.size()-1; i >= 0; i--) {
+            if (listOne.contains(listTwo.get(i))){
+                listTwo.remove(i);
+            }
+        }
+        print(listOne, listTwo);
+    }
+
+    static void print(ArrayList listOne, ArrayList listTwo) {
+        System.out.println("listOne : " + listOne);
+        System.out.println("listTwo : " + listTwo);
+        System.out.println();
+    }
+}
