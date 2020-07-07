cbea788bb492814b6aa7a466623f6982182881ca
keunykyeongKim <woodeekim@gmail.com>
2020-07-07 23:57:09 +0900

    [java-basic] array

---
+++ java-basic/src/main/java/com/woodee/algorithm/array/DayOfYear.java
@@ -0,0 +1,39 @@
+package com.woodee.algorithm.array;
+
+import java.util.Scanner;
+
+public class DayOfYear {
+    // 각 달의 일수
+    static int[][] mdays = {
+            {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, //평년
+            {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, //윤년
+    };
+
+    // 서기 year년은 윤년인가? (윤년:1 / 평년:0)
+    static int isLeap(int year) {
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? 1 : 0);
+    }
+
+    // 서기 y년 m월 d일의 그 해 경과 일 수를 구함
+    static int dayOfYear(int y, int m, int d) {
+        int days = d; // 일 수
+
+        for (int i = 1; i < m; i++) { //1월~(m-1)월의 일 수를 더함
+            days += mdays[isLeap((y))][i - 1];
+        }
+        return days;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int retry;
+
+        System.out.println("그 해 경과 일수를 구합니다");
+
+        do {
+            System.out.print("년"); int year = scanner.nextInt();
+            System.out.print("월"); int month = scanner.nextInt();
+            System.out.print("일"); int day = scanner.nextInt();
+        }while (retry==1);
+    }
+}
---
+++ java-basic/src/main/java/com/woodee/algorithm/array/Int2DArray.java
@@ -0,0 +1,17 @@
+package com.woodee.algorithm.array;
+
+public class Int2DArray {
+    public static void main(String[] args) {
+        int[][] x = new int[2][4]; // 2차원 배열 선언
+
+        x[0][1] = 37;
+        x[0][3] = 54;
+        x[1][2] = 65;
+
+        for(int i=0; i<2; i++){ // 각 요소 출력
+            for(int j=0; j<4; j++){
+                System.out.println("x["+i+"]"+"["+j+"]="+x[i][j]);
+            }
+        }
+    }
+}
