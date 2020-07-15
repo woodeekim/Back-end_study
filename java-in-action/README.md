8a45d11325025d27081b81a28a7620e9c0fe067e
keunykyeongKim <woodeekim@gmail.com>
2020-07-16 08:23:41 +0900

    [java-in-action] Start

---
+++ java-in-action/README.md
@@ -0,0 +1,5 @@
+## 모던 자바 인 액션 (Modern Java in Action)
+
+```java
+[Start] 2020-07-16 Thur
+```
---
+++ java-in-action/src/main/java/org/woodee/Main.java
@@ -0,0 +1,7 @@
+package org.woodee;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java in Action");
+    }
+}
